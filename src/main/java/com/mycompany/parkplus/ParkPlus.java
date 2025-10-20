30f1574b0f1136839f8ebe4aadf304553a1bd5ee
Daniel <>
2025-10-19 19:39:33 -0600

    Carga de prueba1

--- src/main/java/com/mycompany/parkplus/ParkPlus.java
+++ src/main/java/com/mycompany/parkplus/ParkPlus.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.parkplus;
 
-/**
- *
- * @author dcuyu
- */
 public class ParkPlus {
 
     public static void main(String[] args) {
