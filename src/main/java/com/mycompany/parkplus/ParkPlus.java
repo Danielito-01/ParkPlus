16702ca9aa436b30e4eaf9fc6185a62aac01d5b3
Daniel <dcuyu@Daniel>
2025-10-22 20:53:56 -0600

    Creacion del menu principal

--- src/main/java/com/mycompany/parkplus/ParkPlus.java
+++ src/main/java/com/mycompany/parkplus/ParkPlus.java
@@ -1,8 +1,27 @@
 package com.mycompany.parkplus;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ParkPlus {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String url = "jdbc:sqlserver://Daniel:1433;databaseName=ParkPlus;encrypt=true;trustServerCertificate=True";
+        String usuario = "danipark";
+        String contra = "Dani01";
+        try {
+            Connection con = DriverManager.getConnection(url,usuario,contra);
+            System.out.println("Conexion exitosa!!");
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            System.out.println("Eror al conectar a la base de datos");
+            System.out.println(ex.getMessage());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        PantallaPark Pantalla = new PantallaPark();
+        Pantalla.setVisible(true);
     }
 }
